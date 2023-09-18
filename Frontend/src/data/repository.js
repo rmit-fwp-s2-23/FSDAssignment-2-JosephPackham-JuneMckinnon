a8fed8d5c6b0e5cbb1d4d03e83fcf5e1fe3c5e07
Joseph Packham <s3838978@student.rmit.edu.au>
2023-09-18 16:32:53 +1000

    added api to the frontend started adding api calls to the frontend

---
+++ Frontend/src/data/repository.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+// --- Constants ----------------------------------------------------------------------------------
+const API_HOST = "http://localhost:4000";
+const USER_KEY = "user";
+
+// --- User -----------------------------------------------------------------------------
+async function findUser(id) {
+    const response = await axios.get(API_HOST + `/api/users/select/${id}`);
+
+    return response.data;
+}
+
+async function createUser(user) {
+    const response = await axios.post(API_HOST + "/api/users", user);
+
+    return response.data;
+}
+
+    async function deleteUser(id) {
+        const response = await axios.delete(API_HOST + `/api/users/${id}`); 
+        return response.data;
+}
+    
+        async function updateUser(id, user) {
+            const response = await axios.put(API_HOST + `/api/users/${id}`, user);
+
+            return response.data;
+        }
+
+// --- Reviews -----------------------------------------------------------------------------
+
+
+
