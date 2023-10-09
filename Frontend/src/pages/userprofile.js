f5c166ac0121f3aca221a113f9bb044ecfd95ecb
junemckinnon <104176613+junemckinnon@users.noreply.github.com>
2023-10-10 02:15:41 +1100

    started converting localstorage to axios added useEffect to generate the user

--- Frontend/src/pages/userprofile.js
+++ Frontend/src/pages/userprofile.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import '../css/userprofile.css'
 import { useNavigate } from "react-router";
+import { findUser } from "../data/repository";
 
 //TODO: styling
 //TODO: add in confirmation fo deletion
@@ -8,16 +9,39 @@ import { useNavigate } from "react-router";
 
 
 const UserProfile = (props) => {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const getUser = async (id) => {
+            try {
+                const fetchedUser = await findUser(id);
+                setUser(fetchedUser);
+            } catch {
+                console.log("error fetching user, using test user instead")
+                const testUser = {
+                    name: "June",
+                    email: "s3947118@student.rmit.edu.au",
+                    joined: "10/10/23"
+                }
+                setUser(testUser);
+            }
+        }
+
+        getUser(1);
+    })
+
     const navigate = useNavigate(); //used to navigate to different pages
     const handleEdit = () => { //when edit button is clicked, navigate to edit profile page
         navigate('/editprofile');
     }
+    
     const handleDelete = () => { //when delete button is clicked, delete account
         if(window.confirm('Are you sure you want to delete your account?') === true){ //confirm deletion
             let users = JSON.parse(localStorage.getItem('users')); //get all registered users from local storage
 
             for(let i = 0; i < users.length; i++){ //loop through all registered users
-            if(users[i].email === props.user.email){ //if user is found
+            if(users[i].email === user.email){ //if user is found
                 users.splice(i, 1); //remove user from array
                 localStorage.setItem('users', JSON.stringify(users)); //update local storage
                 localStorage.removeItem('loggedUser'); //remove logged user from local storage
@@ -40,17 +64,17 @@ const UserProfile = (props) => {
             <div className = "content">
                 <div className = "background">
                     <div id = "header">
-                        {props.user.name}'s Profile
+                        {user.name}'s Profile
                     </div>
                     <br></br>
                     <div className = "small-background">
-                        Username: {props.user.name}
+                        Username: {user.name}
                     </div>
                     <div className = "small-background">
-                        Email: {props.user.email}
+                        Email: {user.email}
                     </div>
                     <div className = "small-background">
-                        Joined: {props.user.joined}
+                        Joined: {user.joined}
                     </div>
                     <div className = "button-container">
                         <button className="button" id = 'edit' onClick = {handleEdit}>Edit</button>
