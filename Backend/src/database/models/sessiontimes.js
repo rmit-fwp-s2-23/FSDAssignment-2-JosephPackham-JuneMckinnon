5a7d2fdc9e88f479fbcb25c5070ed44b0c054301
Joseph Packham <s3838978@student.rmit.edu.au>
2023-10-12 12:25:25 +1100

    started on movie and session time tables

--- Backend/src/database/models/sessiontimes.js
+++ Backend/src/database/models/sessiontimes.js
@@ -1,2 +1,30 @@
 module.exports = (sequelize, DataTypes) =>
-    sequelize
+    sequelize.define('sessiontimes', {
+        sessiontime_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        sessiontime_movie: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        
+        sessiontime_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        sessiontime_time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        sessiontime_available_seats: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        sessiontime_total_seats: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+       
+    });
