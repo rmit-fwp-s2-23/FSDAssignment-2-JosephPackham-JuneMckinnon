d49e07187699e1968794227c7a1f9d1fd1407641
Joseph Packham <s3838978@student.rmit.edu.au>
2023-10-12 21:16:40 +1100

    added comment

--- Backend/src/database/index.js
+++ Backend/src/database/index.js
@@ -56,7 +56,7 @@ async function seedData() {
 
   
 };
-
+//This is sample data for the assignment, This was the data hardcoded in assignment one
 async function seedAssignmentData() {
     const count = await db.movies.count();
     if(count > 1) {
