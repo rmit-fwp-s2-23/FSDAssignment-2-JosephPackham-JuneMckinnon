ffcd313fb7748d3514feac5fc906c3348fadb6b1
junemckinnon <104176613+junemckinnon@users.noreply.github.com>
2023-10-13 22:38:51 +1100

    testing page for testing features

--- Admin/admin/src/Routing/routing.js
+++ Admin/admin/src/Routing/routing.js
@@ -1,12 +1,14 @@
 import {Route, Routes} from "react-router-dom"; //import Route and Routes components from react-router-dom
 import Login from "../pages/login";
 import Home from "../pages/home";
+import Test from "../pages/test";
 
 const Routing = (props) => { 
     return (
         <Routes>
             <Route path = "/" element = { <Login /> } />
             <Route path = "/home" element = { <Home /> } />
+            <Route path = "/test" element = { <Test /> } />
         </Routes>
     );
 };
---
+++ Admin/admin/src/pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../css/test.css";
+
+const Test = () => {
+    return (
+        <div className = "outer">
+            <div className = "inner"></div>
+        </div>
+    );
+}
+
+export default Test;
