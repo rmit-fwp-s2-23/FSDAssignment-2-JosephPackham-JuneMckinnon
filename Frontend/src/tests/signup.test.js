ae3e01d7ab6d62aae151ed1a75b85cda629ac859
Joseph Packham <s3838978@student.rmit.edu.au>
2023-10-15 23:00:08 +1100

    Update signup.test.js

--- Frontend/src/tests/signup.test.js
+++ Frontend/src/tests/signup.test.js
@@ -7,42 +7,46 @@ import { findUserByEmail } from '../data/repository';
 import SignUp from '../pages/signup';
 import { createUser } from '../data/repository';
 
+// Start the mock server
 server.listen();
 
-
-
+// Define the mock localStorage object
 const mockLocalStorage = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    removeItem: jest.fn(),
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
 };
 
+// Set the global localStorage object to the mockLocalStorage object
 Object.defineProperty(global, 'localStorage', { value: mockLocalStorage });
 
 test('renders Sign Up page with correct information', async () => {
+  // Set the mock localStorage object to return a mock user object
+  mockLocalStorage.getItem.mockReturnValue(
+    JSON.stringify({
+      email: 'june@email.com',
+    })
+  );
 
-    mockLocalStorage.getItem.mockReturnValue(JSON.stringify({ email: 'june@email.com'}));
+  // Render the SignUp component with the mock localStorage object
+  render(
+    <MemoryRouter>
+      <SignUp />
+    </MemoryRouter>
+  );
 
-    render(
-        <MemoryRouter>
-            <SignUp />
-        </MemoryRouter>
-    )
+  // Wait for the component to fetch user data
+  await screen.findByText(/Welcome to Loop Cinemas!/i);
 
-    // Wait for the component to fetch user data
-    await screen.findByText(/Welcome to Loop Cinemas!/i);
+  // Assert that user information is displayed on the page
+  expect(screen.getByText(/Welcome to Loop Cinemas!/i)).toBeInTheDocument();
+  expect(screen.getByTestId('Name')).toBeInTheDocument();
+  expect(screen.getByTestId('Email')).toBeInTheDocument();
+  expect(screen.getByTestId('Password')).toBeInTheDocument();
+  expect(screen.getByTestId('SignUp')).toBeInTheDocument();
+});
 
-    // Assert that user information is displayed on the page
-    expect(screen.getByText(/Welcome to Loop Cinemas!/i)).toBeInTheDocument();
-    expect(screen.getByTestId('Name')).toBeInTheDocument();
-    expect(screen.getByTestId('Email')).toBeInTheDocument();
-    expect(screen.getByTestId('Password')).toBeInTheDocument();
-    expect(screen.getByTestId('SignUp')).toBeInTheDocument();
-
-
-  });
-
- 
+// Close the mock server after all tests have run
 afterAll(() => {
-    server.close();
-} );
+  server.close();
+});
