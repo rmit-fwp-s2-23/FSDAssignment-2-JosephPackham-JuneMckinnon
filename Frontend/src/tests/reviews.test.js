5be41967bb346b2950a0cd4b35b7dde2680cf883
Joseph Packham <s3838978@student.rmit.edu.au>
2023-10-15 22:57:46 +1100

    Update reviews.test.js

--- Frontend/src/tests/reviews.test.js
+++ Frontend/src/tests/reviews.test.js
@@ -4,30 +4,29 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Reviews from '../pages/reviews';
 import { getReviewsByMovie } from '../data/repository';
-import '@testing-library/jest-dom';
 import { server } from './mocks/server.js';
 
+// Start the mock server
 server.listen();
 
 test('renders reviews for a movie', async () => {
-    // Define the mock props
-    const movie = 'Test Movie';
-    const user = {
-      name: 'Test User',
-      email: 'test@example.com',
-    };
+  // Define the mock props
+  const movie = 'Test Movie';
+  const user = {
+    name: 'Test User',
+    email: 'test@example.com',
+  };
 
-    window.prompt = jest.fn().mockReturnValue('Test review text');
-    window.alert = jest.fn();
+  // Define the mock window.prompt and window.alert functions
+  window.prompt = jest.fn().mockReturnValue('Test review text');
+  window.alert = jest.fn();
 
-    //get reviews from database by movie
-    const reviews = await getReviewsByMovie(movie);
+  // Get the reviews from the database by movie
+  const reviews = await getReviewsByMovie(movie);
 
-
-
-  //check if reviews are defined
-    expect(reviews).toBeDefined();
-    expect(reviews.length).toBeGreaterThan(0);
+  // Check if the reviews are defined and have a length greater than 0
+  expect(reviews).toBeDefined();
+  expect(reviews.length).toBeGreaterThan(0);
 
   // Render the Reviews component with the mock props
   render(
@@ -36,13 +35,11 @@ test('renders reviews for a movie', async () => {
     </MemoryRouter>
   );
 
-    // Wait for the reviews to be rendered
-    await waitFor(() => {
-        const reviewElements = screen.getAllByTestId('review');
-        expect(reviewElements.length).toBe(reviews.length);
-      });
-
-
+  // Wait for the reviews to be rendered
+  await waitFor(() => {
+    const reviewElements = screen.getAllByTestId('review');
+    expect(reviewElements.length).toBe(reviews.length);
+  });
 
   // Simulate a click on the Edit button for the first review
   fireEvent.click(screen.getAllByText('Edit')[0]);
@@ -53,13 +50,11 @@ test('renders reviews for a movie', async () => {
   // Simulate a click on the Delete button for the second review
   fireEvent.click(screen.getAllByText('Delete')[1]);
 
-  //Assert that the review can only be deleted by the user who created it
+  // Assert that the review can only be deleted by the user who created it
   expect(window.alert).toBeCalledWith('You can only delete your own reviews');
-
-
 });
 
-//after all close server
+// Close the mock server after all tests have run
 afterAll(() => {
   server.close();
-} );
+});
