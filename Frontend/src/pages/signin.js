e682f339d0ea6dadfb45123abb58ee83e8e869be
Joseph Packham <s3838978@student.rmit.edu.au>
2023-10-15 22:08:27 +1100

    sign in comments

--- Frontend/src/pages/signin.js
+++ Frontend/src/pages/signin.js
@@ -4,14 +4,11 @@ import { useNavigate } from "react-router";
 import { useState } from "react";
 import { findUserByEmail, verifyUser } from "../data/repository";
 
-
-
-
-
-
+// Define the SignIn component
 const SignIn = (props) => {
     const navigate = useNavigate(); //used to navigate to different pages
 
+    // Define the handleSubmit function
     const handleSubmit = async (e) => {
         e.preventDefault(); //prevents page from refreshing
         const uservalues = { //create user object with email and password
@@ -31,42 +28,9 @@ const SignIn = (props) => {
         } else {
             document.getElementById('error').innerText = "Email or Password is incorrect";
         }
-
-
-
-      
-        
     }
 
-        //generate all the users that have already signed up
-        // const users = JSON.parse(localStorage.getItem('users'))
-        // //loop through array of users to check if users details are correct, then log in
-        // for (let i = 0; i < users.length; i++){
-        //     if (user.email === users[i].email && user.password === users[i].password){ //if user email and password match a user in the array
-        //         localStorage.setItem('loggedUser', JSON.stringify(users[i])) //set loggedUser in local storage to logged in user
-        //         props.setUser({name: users[i].name, email: users[i].email, password: users[i].password }); //set user state to logged in user
-        //         alert('Logged in successfully! Welcome back ' + users[i].name)
-        //         navigate('/userprofile'); //navigate to landing page
-        //         break;
-                
-        //     //if user email and password don't match a user in the array
-        //     } else {
-        //         document.getElementById('error').innerText = "Email or Password is incorrect";
-
-        //     }
-            
-                
-        
-        
-
-
-
-  
-
-
-
-    //return sign in page
-
+    // Define the SignIn component's return statement
     return (
         <div className="page">
             <div className = "content">
@@ -94,23 +58,3 @@ const SignIn = (props) => {
 
 export default SignIn;
 
-/* 
-
-<div>
-    <h1 className="heading">S</h1>
-</div>
-<form onSubmit={e => handleSubmit(e)}>
-<div>
-    <input className="input" type = 'email' id="email" placeholder="Email"></input>
-</div>
-<div>
-    <input className="input" type = 'password' id = 'password' placeholder="Password"></input>
-</div>
-<div>
-    <button className="button" id = 'signin' type="submit"> Sign In</button>
-</div>
-<div id = {'error'}></div>
-</form>
-    
-
-*/
