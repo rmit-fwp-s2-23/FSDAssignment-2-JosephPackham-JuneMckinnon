6625357b4cc64b41d972cced85fc4c9ee65a9abb
junemckinnon <104176613+junemckinnon@users.noreply.github.com>
2023-10-15 22:04:25 +1100

    create page done except connection to backend

--- Admin/admin/src/components/createmovie.js
+++ Admin/admin/src/components/createmovie.js
@@ -17,7 +17,7 @@ const createReducer = (prevState, action) => {
         case 'SESSION':
             return {
                 ...prevState,
-                isLoggedIn: true,
+                sessiontimes: [...prevState.sessiontimes, action.payload],
             };
         default:
             break;
@@ -43,16 +43,50 @@ const CreateMovie = () => {
     }
 
     const handleCreate = (e) => {
-        console.log(state.movie_name, state.movie_image)
+        console.log(state.movie_name, state.movie_image, state.sessiontimes)
+    }
+
+    const createSession = (e) => {
+        e.preventDefault();
+        const session = {
+            sessiontime_time: e.target[0].value,
+            sessiontime_day: e.target[1].value,
+            sessiontime_available_seats: e.target[2].value
+        }
+        dispatcher({ type: 'SESSION', payload: session });
     }
 
     return (
         <div className = "create-bkg">
-            <label className = "create-label" htmlFor = "create-name">Movie Name</label>
-            <input className = "create-input" id = "create-name" onChange={(e) => handleName(e)}></input>
-            <label className = "create-label" htmlFor = "create-name">Movie Image</label>
-            <input className = "create-input" id = "create-name" onChange={(e) => handleImage(e)}></input>
-            <button className = "create-button" onClick = {handleCreate}>Create Movie</button>
+            <div className = "create-left">
+                <label className = "create-label" htmlFor = "create-name">Movie Name</label>
+                <input className = "create-input" id = "create-name" onChange={(e) => handleName(e)}></input>
+                <label className = "create-label" htmlFor = "create-name">Movie Image</label>
+                <input className = "create-input" id = "create-name" onChange={(e) => handleImage(e)}></input>
+                <button className = "create-button" onClick = {handleCreate}>Create Movie</button>
+            </div>
+            <div className = "create-right">
+                <form className = "create-top" onSubmit = {(e) => {createSession(e)}}>
+                    <label className = "create-session-label" htmlFor = "create-session-name">Session Time</label>
+                    <input className = "create-session-input" id = "create-session-name"></input>
+                    <label className = "create-session-label" htmlFor = "create-session-name">Session Day</label>
+                    <input className = "create-session-input" id = "create-session-name"></input>
+                    <label className = "create-session-label" htmlFor = "create-session-name">Session Seats</label>
+                    <input className = "create-session-input" id = "create-session-name"></input>
+                    <input type = "submit" value = "Submit" className = "session-submit"></input>
+                </form>
+                <div className = "create-bottom">
+                    {state.sessiontimes.map(session =>
+                        <div className = "grid-item">
+                            <div className = "session-time">
+                                <div>Day: {session.sessiontime_day}</div>
+                                <div>Time: {session.sessiontime_time}</div>
+                                <div>Seats: {session.sessiontime_available_seats}</div>
+                            </div>   
+                        </div> 
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
